4a7a2ce562b9ca63d433ca7399fd572edbc55eec
Kavya <onlyproductivehere@gmail.com>
2023-05-25 00:53:54 +0545

    Made working etch-a-sketch

---
+++ Front End Practice/etch-a-sketch/script.js
@@ -0,0 +1,81 @@
+let setSizeBtn = document.querySelector('.set-size button');
+let gridSize = 16;
+let selectedColor = "red";
+
+let sketchArea = document.querySelector('.main-container');
+
+createDiv(gridSize);
+
+setSizeBtn.addEventListener('click', () => {
+    console.log(gridSize);
+    let tempGridSize = +document.getElementById('grid-size').value;
+    if ((tempGridSize >= 1 && tempGridSize <= 100)) {
+        gridSize = tempGridSize;
+    }
+    clearScreen();
+    createDiv(gridSize);
+})
+
+let colorList = document.querySelectorAll('label > input');
+
+colorList.forEach((color) => {
+    color.addEventListener('click', () => {
+        selectedColor = color.value;
+    })
+})
+// randomColorOption = colorList[colorList.length-1];
+// randomColorOption.addEventListener('click',()=>{
+//     selectedColor= getRandomColor();
+// });
+
+function createDiv(num) {
+    let rowContainer, block;
+    // each row of the grid is a container for smaller blocks
+    for (let i = 0; i < num; i++) {
+        rowContainer = document.createElement('div');
+        rowContainer.style.height = `${(100 / gridSize)}%`;
+        rowContainer.style.width = '100%';
+        rowContainer.setAttribute("class", "each-div-line")
+        for (let j = 0; j < num; j++) {
+            block = document.createElement("div");
+            block.style.width = `${100 / gridSize}%`;
+            block.setAttribute("class", "box")
+            rowContainer.appendChild(block);
+        }
+        sketchArea.appendChild(rowContainer);
+    }
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            if (selectedColor==='random'){
+                box.style.backgroundColor = `${getRandomColor()}`;
+            }
+            box.style.backgroundColor = `${selectedColor}`;
+        })
+    })
+}
+
+function clearScreen() {
+    sketchArea.innerHTML = "";
+}
+
+function getRandomColor(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+
+console.log(getRandomColor());
+
+let resetBtn = document.querySelector('.color-reset');
+resetBtn.addEventListener('click',()=>{
+    clearScreen();
+    createDiv(gridSize);
+})
+
+
+
+
+
+
