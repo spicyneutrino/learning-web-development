415cf4544e9141e3224842b70473a2c621890902
Kavya <onlyproductivehere@gmail.com>
2023-05-26 22:42:17 +0545

    Add keyboard support ,Restructure code

--- Front End Practice/calculator/script.js
+++ Front End Practice/calculator/script.js
@@ -14,13 +14,13 @@ let dotBtn = document.querySelector(".dot-button");
 let equalBtn = document.querySelector(".equal-button");
 
 // variables
-//top and botttom area of display
+//top and bottom area of display
 let prevCalc = 0;
 let currentCalc = 0;
 let operator = "";
 let previousOperator = "=";
 
-//events
+//events for mouse
 
 numbers.forEach((btn) => {
     btn.addEventListener('click', () => {
@@ -28,34 +28,52 @@ numbers.forEach((btn) => {
     })
 })
 
-dotBtn.addEventListener('click',()=>{
-    if (!currentCalculationArea.textContent.includes('.')){
+dotBtn.addEventListener('click', () => {
+    if (!currentCalculationArea.textContent.includes('.')) {
         currentCalculationArea.textContent += ".";
     }
 })
-operators.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        operator = btn.textContent;
-        if (previousOperator != '=') {
-            currentCalc = +currentCalculationArea.textContent;
-            currentCalculationArea.textContent = "";
-            prevCalc = +operate(prevCalc, previousOperator, currentCalc)
-            prevCalculationArea.textContent = prevCalc + operator;
-        } else {
-            prevCalculationArea.textContent = currentCalculationArea.textContent + operator;
-            prevCalc = +currentCalculationArea.textContent;
-            currentCalculationArea.textContent = "";
-        }
-        previousOperator = operator;
 
-    })
+operators.forEach((btn) => {
+    btn.addEventListener('click', ()=>{
+        operatorBtnClickAction(btn);
+    });
 })
 
 clearBtn.addEventListener('click', clearScreen);
 deleteBtn.addEventListener('click', deleteScreen);
 equalBtn.addEventListener('click', operateAndUpdate);
 
+// events for keyboard
 
+window.addEventListener('keydown', (e) => {
+    
+    numbers.forEach((btn) => {
+        if (btn.textContent===e.key){
+            currentCalculationArea.textContent += btn.textContent;
+        }
+    })
+
+    if (e.key==="."){
+        if (!currentCalculationArea.textContent.includes('.')) {
+            currentCalculationArea.textContent += ".";
+        }
+    }
+
+    if (e.key==="Enter"){
+        operateAndUpdate();
+    }
+    if (e.key ==="Delete" || e.key ==="Backspace"){
+        deleteScreen();
+    }
+    if (e.key.toLowerCase() ==="c"){
+        clearScreen();
+    }
+
+    e.preventDefault();
+})
+
+// helper functions
 
 function operate(num1, operator, num2) {
     switch (operator) {
@@ -66,7 +84,7 @@ function operate(num1, operator, num2) {
         case "*":
             return +num1 * +num2;
         case "÷":
-            if (num2==0){
+            if (num2 == 0) {
                 alert('Can not divide by zero ¯\\_(ツ)_/¯');
                 return 0;
             }
@@ -87,21 +105,30 @@ function deleteScreen() {
     currentCalc = currentCalculationArea.textContent;
 }
 
-function updateCurrentCalc(text) {
-    currentCalculationArea.textContent = text;
-}
-function updatePrevCalc(text) {
-    prevCalculationArea.textContent = text;
-}
-
 function operateAndUpdate() {
     currentCalc = +currentCalculationArea.textContent;
     // currentCalc = operate(prevCalc,operator,currentCalc);
-    if (previousOperator=='='){
+    if (previousOperator == '=') {
         prevCalculationArea.textContent = currentCalc;
     } else {
         prevCalculationArea.textContent += currentCalc + "=";
     }
     currentCalculationArea.textContent = operate(prevCalc, operator, currentCalc);
     previousOperator = "=";
+}
+
+function operatorBtnClickAction(btn) {
+    operator = btn.textContent;
+    if (previousOperator != '=') {
+        currentCalc = +currentCalculationArea.textContent;
+        currentCalculationArea.textContent = "";
+        prevCalc = +operate(prevCalc, previousOperator, currentCalc)
+        prevCalculationArea.textContent = prevCalc + operator;
+    } else {
+        prevCalculationArea.textContent = currentCalculationArea.textContent + operator;
+        prevCalc = +currentCalculationArea.textContent;
+        currentCalculationArea.textContent = "";
+    }
+    previousOperator = operator;
+
 }
