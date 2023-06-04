09e84955be86a2868a4d5ab0990c71cc772ef7dc
Kavya <onlyproductivehere@gmail.com>
2023-06-04 12:08:43 +0545

    Add: DOM updates gameboard array

--- Front End Practice/tic-tac-toe/script.js
+++ Front End Practice/tic-tac-toe/script.js
@@ -7,38 +7,12 @@ let gameboard = [
 function player(marker) {
 
     let addMarker = (tile) => {
-        if (tile.textContent.length ===1) {
+        if (tile.textContent.length === 1) {
             return;
         }
         tile.textContent = marker;
 
-        (function updateGameBoard() {
-            tiles.forEach(tile => {
-                //if tile is empty or it has any marker already in it dont update game board
-                               
-
-                let tileIndex = Array.from(tiles).indexOf(tile);
-
-                let boardRow = 0;
-                let boardCol = 0;
-                if (tileIndex < 4) {
-                    boardRow = 0;
-                    boardCol = tileIndex - 1;
-                } else if (tileIndex < 7) {
-                    boardRow = 1;
-                    boardCol = tileIndex - 4;
-                    // -3 for  each row
-                    // -1 for index/position offset 
-                } else {
-                    boardRow = 2;
-                    boardCol = tileIndex - 7;
-                    // -6 for 2 rows
-                    // -1 for index/position offset
-                }
-                gameboard[boardRow][boardCol] = marker;
-            })
-        })(marker);
-
+        updateGameBoard(tile, marker);
     }
 
     return { addMarker };
@@ -58,7 +32,52 @@ tiles.forEach(tile => {
             player2.addMarker(tile);
         }
         firstUser = !firstUser;
+        console.log(gameboard);
     })
-    
+
 })
+
+function updateGameBoard(tile, marker) {
+    let tileIndex = Array.from(tiles).indexOf(tile);
+    // 1 not subtracted as we are taking index of the array into account not position on the nodelist
+    //position on node list
+    //     1 2 3
+    //     4 5 6
+    //     7 8 9
+    // index on array
+    //     0 1 2
+    //     3 4 5
+    //     6 7 8
+
+    let boardRow = 0;
+    let boardCol = 0;
+    if (tileIndex < 3) {
+        boardRow = 0;
+        boardCol = tileIndex;
+    } else if (tileIndex < 6) {
+        boardRow = 1;
+        boardCol = tileIndex -3;
+        // -3 for  each row
+
+    } else {
+        boardRow = 2;
+        boardCol = tileIndex - 6;
+        // -6 for 2 rows
+    }
+    gameboard[boardRow][boardCol] = marker;
+
+};
+
+function checkGame() {
+    let checkRow = ((gameboard[0][0] === gameboard[0][1]) === gameboard[0][2]) || ((gameboard[1][0] === gameboard[1][1]) === gameboard[1][2]) || ((gameboard[2][0] === gameboard[2][1]) === gameboard[2][2]);
+    let checkCol = ((gameboard[0][0] === gameboard[1][0]) === gameboard[2][0]) || ((gameboard[0][1] === gameboard[1][1]) === gameboard[2][1]) || ((gameboard[0][2] === gameboard[1][2]) === gameboard[2][2]);
+    let checkDiagonal = (gameboard[0][0] === gameboard[1][1]) === gameboard[2][2];
+
+    if (checkRow || checkCol) {
+        console.log("GAME");
+    }
+    if (checkDiagonal) {
+        console.log("DIAGONAL GAME");
+    }
+}
 
